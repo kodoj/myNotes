1a8cd4b751e7628e0dbce2e1a6d56f52a3fd4761
kodoj <kodoj1@o2.pl>
2018-12-24 13:56:29 +0100

    app works fine, now refactoring for better code quality

--- myNotes/main.js
+++ myNotes/main.js
@@ -5,12 +5,19 @@
         onDragStart,
         onDrag,
         onDragEnd,
+        getNoteObject,
         grabPointY,
         grabPointX,
         createNote,
-        addNoteBtnEl;
+        addNoteBtnEl,
+        onAddNoteBtnClick,
+        saveNote,
+        deleteNote,
+        loadNotes,
+        init,
+        testLocalStorage;
     
-    onDragStart = function (ev) {
+    const onDragStart = function (ev) {
       var boundingClientRect;
       if (ev.target.className.indexOf('bar') === -1) {
         return;
@@ -40,7 +47,7 @@
         posY = 0;
       }
       
-      draggedEl.style.transform = "translateX(" + posX + "px) translateY(" + posY + "px)";
+      draggedEl.style.transform = "translateX("+ posX + "px) translateY(" + posY + "px)"; //template strings
     };
     
     onDragEnd = function () { 
@@ -48,35 +55,134 @@
       grabPointX = null;
       grabPointY = null;
     };
+  
+    getNoteObject = function (el) {
+      var textarea = el.querySelector('textarea'),
+          title = el.querySelector('.title');
+
+      return (
+      {
+        transformCSSValue: el.style.transform,
+        content: textarea.value,
+        title: title.value,
+        id: el.id,
+        textarea: {
+          width: textarea.style.width,
+          height: textarea.style.height,
+        }
+      });
+    };
     
-    createNote = function () {
+    onAddNoteBtnClick = function () {
+      createNote();
+    };
+    
+    createNote = function (options) {
       var stickerEl = document.createElement('div'),
           barEl = document.createElement('div'),
+          deleteBtnEl = document.createElement('button'),
           textareaEl = document.createElement('textarea'),
-          textareaTitle = document.createElement('textarea');
+          textareaTitle = document.createElement('textarea'),
+          BOUNDARIES = 400,
+          noteConfig = options || {
+            transformCSSValue: "translateX(" + Math.random() * BOUNDARIES + "px) translateY(" + Math.random() * BOUNDARIES + "px)",
+            content: '',
+            title: '',
+            id: "sticker_" + new Date().getTime(),
+          },
+          onSave,
+          onDelete;
       
-      var transformCSSValue = "translateX(" + Math.random() * 400 + "px) translateY(" + Math.random() * 400 + "px)";
+      if (noteConfig.textarea) {
+        textareaEl.style.width = noteConfig.textarea.width;
+        textareaEl.style.height = noteConfig.textarea.height;
+        textareaEl.style.resize = 'none';
+      }
       
-      stickerEl.style.transform = transformCSSValue; 
+      onSave = function () {
+        saveNote(getNoteObject(stickerEl));
+      };
+  
+      onDelete = function () {
+        deleteNote(getNoteObject(stickerEl));
+  
+        document.body.removeChild(stickerEl);
+      };
+      
+      stickerEl.style.transform = noteConfig.transformCSSValue; 
+      stickerEl.id = noteConfig.id;
+      textareaEl.value = noteConfig.content;
+      textareaTitle.value = noteConfig.title;
+      
+      deleteBtnEl.classList.add('deleteButton');
+      deleteBtnEl.addEventListener('click', onDelete, false);
       
       barEl.classList.add('bar');
       stickerEl.classList.add('sticker');
-      textareaEl.classList.add('mainText');
       textareaTitle.classList.add('title');
       
+      barEl.appendChild(deleteBtnEl);
+      
       stickerEl.appendChild(barEl);
-      stickerEl.appendChild(textareaTitle);
-      stickerEl.appendChild(textareaEl); 
+      stickerEl.appendChild(textareaEl);
+      stickerEl.appendChild(textareaTitle); 
       
       stickerEl.addEventListener('mousedown', onDragStart, false);
+      stickerEl.addEventListener('focusout', onSave, false);
+      deleteBtnEl.addEventListener('click', deleteNote);
       
       document.body.appendChild(stickerEl);
     };
     
-    createNote(); 
+
+    testLocalStorage = function () {
+      var test = 'test';
+      try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    };
     
-    addNoteBtnEl = document.querySelector('.addNoteBtn');
-    addNoteBtnEl.addEventListener('click', createNote, false);
-    document.addEventListener('mousemove', onDrag, false);
-    document.addEventListener('mouseup', onDragEnd, false);
+    init = function () {
+      
+      if (!testLocalStorage) {
+        var message = "We are sorry but you cannot use localStorage";
+        saveNote = function () {
+          console.warn(message);
+        };
+        
+        deleteNote = function () {
+          console.warn(message);
+        };
+      } else {
+        saveNote = function (note) {
+          localStorage.setItem(note.id, JSON.stringify(note));
+        };
+  
+        deleteNote = function (note) {
+          localStorage.removeItem(note.id);
+        };
+  
+        loadNotes = function () {
+          for(var i = 0; i < localStorage.length; i++) {
+            var noteObject = JSON.parse(
+              localStorage.getItem(localStorage.key(i))
+            );
+            createNote(noteObject);
+          };
+        };
+        loadNotes();      
+      }
+  
+      addNoteBtnEl = document.querySelector('.addNoteBtn');
+      addNoteBtnEl.addEventListener('click', onAddNoteBtnClick, false);
+      document.addEventListener('mousemove', onDrag, false);
+      document.addEventListener('mouseup', onDragEnd, false);
+    };
+    
+    init();
+    
   })();
